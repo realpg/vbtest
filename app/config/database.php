b18fc794b3de9dabf848c286ebe3aeaca5c779ff
realpg <qshenx@foxmail.com>
2016-06-19 15:15:32 +0800

    db

--- app/config/database.php
+++ app/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => 'localhost:/var/run/mysqld/mysqld.sock',
 	'username' => 'v2buy',
 	'password' => 'hFhukAfXvVArd9IE',
 	'database' => 'v2buy',
