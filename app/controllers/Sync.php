84ce36d0e14abbaf5a9f7903857448c7dd6771d4
realpg <qshenx@foxmail.com>
2016-06-19 16:12:33 +0800

    sync

--- app/controllers/Sync.php
+++ app/controllers/Sync.php
@@ -1,6 +1,10 @@
 <?php
 class Sync extends CI_Controller {
+    private $bitcoin;
+    public function __construct() {
+        parent::__construct();
+    }
     public function get_some() {
-
+        $this->load->helper('bitcoin');
     }
 }
