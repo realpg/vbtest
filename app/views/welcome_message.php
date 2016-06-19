d6f97afd471ea5b0ac748671c22be4a514a202d2
realpg <qshenx@foxmail.com>
2016-06-19 15:33:54 +0800

    status

--- app/views/welcome_message.php
+++ app/views/welcome_message.php
@@ -1,8 +1,13 @@
-Q+Coin Golang-Wallet for Linux 
-Version: trunk (build 20160607)
-Non-GUI Server mode
+<html>
+<head><title>wallet status</title></head>
+<body>
+Q+Coin Golang-Wallet for Linux<br>
+Version: trunk (build 20160607)<br>
+Non-GUI Server mode<br>
 
-Status:
-	Mining: false
-	Peers connected: 14
-	TinyWallet connected: Error!(do not log when clients more than 500, for performance)
+Status:<br>
+	Mining: false<br>
+	Peers connected: 14<br>
+	TinyWallet connected: Error!(do not log when clients more than 500, for performance)<br>
+</body>
+</html>
