d91b772987554438c0cdecf44ae9c85ef92b3b73
realpg <qshenx@foxmail.com>
2016-06-19 23:10:36 +0800

    update

--- app/controllers/Data.php
+++ app/controllers/Data.php
@@ -1,11 +1,14 @@
 <?php
 class Data extends CI_Controller {
     public function ins() {
-        $sql = "INSERT INTO qpc_trans (f160,t160,amount,req_time,acc_time) VALUES (?, ?, ?, ?, ?)";
         for ($i=1;$i<1000;$i++) {
             $r1=sha1(microtime() . rand(1,99999999));
             $r2=sha1(microtime() . rand(1,99999999));
-            $this->db->query($sql, array("0x{$r1}", "0x{$r2}",rand(1,9999),time(),time()+rand(1,3600)));
+            $a=rand(1,9999);
+            $t1=time();
+            $t2=time()+rand(1,3600);
+            $sql = "INSERT INTO qpc_trans (f160,t160,amount,req_time,acc_time) VALUES (0x{$r1}, 0x{$r2}, {$a}, {$t1}, {$t2})";
+            $this->db->query($sql);
         }
         die("OK:{$i}");
     }
