b8e4e43339b8ebac421fe5d3fc8804f7ca89b91a
realpg <qshenx@foxmail.com>
2016-06-20 17:06:48 +0800

    1

--- app/models/Blockchain.php
+++ app/models/Blockchain.php
@@ -6,7 +6,7 @@ class Blockchain extends CI_Model {
     
     public function fetch100($lastid) {
         $this->db->where('id >',$lastid);
-        $this->db->order_by('id',asc);
+        $this->db->order_by('id','asc');
         $this->db->limit(100);
         $query=$this->db->get('qpc_trans');
         if ($query->num_rows()>0) {
